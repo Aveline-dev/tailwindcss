23f38a1cd2d419c6f584c81596dab293682c0a4a
AVA's HP <khobeve@gmail.com>
2022-08-03 10:09:13 +0100

    my first commit

---
+++ tailwind.js
@@ -0,0 +1,24 @@
+let changeName = document.getElementById("image 1");
+changeName.addEventListener("click", (event)=>{event.target.style.background = "pink"
+event.target.style.color="beige"});
+let changeName2 = document.getElementById("image 2");
+changeName2.addEventListener("click", (event)=>{event.target.style.background = "red"
+event.target.style.color="white"});
+let changeName3 = document.getElementById("image 3");
+changeName3.addEventListener("click", (event)=>{event.target.style.background = "orange"
+event.target.style.color="green"});
+let changeName4 = document.getElementById("image 4");
+changeName4.addEventListener("click", (event)=>{event.target.style.background = "yellow"
+event.target.style.color="brown"});
+let changeName5 = document.getElementById("image 5");
+changeName5.addEventListener("click", (event)=>{event.target.style.background = "blue"
+event.target.style.color="white"});
+let changeName6 = document.getElementById("image 6");
+changeName6.addEventListener("click", (event)=>{event.target.style.background = "brown"
+event.target.style.color="black"});
+let changeName7 = document.getElementById("image 7");
+changeName7.addEventListener("click", (event)=>{event.target.style.background = "gold"
+event.target.style.color="gray"});
+let changeName8 = document.getElementById("image 8");
+changeName8.addEventListener("click", (event)=>{event.target.style.background = "pink"
+event.target.style.color="beige"});
